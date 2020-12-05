fb468f64098b0b26495ac88a553e50c3f770c8e3
jared <jrdazriel@gmail.com>
2020-12-05 15:18:25 -0500

    update hello_gui_world.py

--- hello_gui_world.py
+++ hello_gui_world.py
@@ -1,3 +1,19 @@
-import tkinter as tk
+#Window Basics
+import tkinter
 
+#Define window
+root = tkinter.Tk()
+root.title('Window Basics')
+root.iconbitmap('')
+root.geometry('720x480')
+root.resizable(1,1)
+root.config(bg='blue')
 
+#second window
+top = tkinter.Toplevel()
+top.title('Window 2')
+top.config(bg='#00c2cb')
+top.geometry('480x480')
+
+#Run root window main loop
+root.mainloop()
